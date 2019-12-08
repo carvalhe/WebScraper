9d035969f3d903bd2c4d8518193e9cb7c2324a6b
Dev Mac <devmac@Devs-MacBook-Pro.local>
2019-12-07 16:34:05 -0800

    Changed Google scraper into Wikipedia scraper

--- Test_Wikipedia_Scraper.py
+++ Test_Wikipedia_Scraper.py
@@ -7,14 +7,15 @@ DEBUG = True
 #### Set up where you are requesting from
 user_input = input("Enter your search string: ")
 # Add the user_input toe the google search url and request that
-search_input = f'https://google.com/search?q={user_input}'
-source = requests.get(search_input)
+search_input = f'https://wikipedia.org/wiki/{user_input}'
+search_input.replace(" ", "_")
+source = requests.get(search_input).text
 
 #### create soup from the page
-soup = BeautifulSoup(source.text, 'lxml')
+soup = BeautifulSoup(source, 'lxml')
 if(DEBUG):
-    print(soup.prettify)
-
+    print(soup.prettify())
 #### Parse through the soup for the information you want
 # The first grab the main elements of the page, div has class=mw
-#main_page = soup.find('div', class_='mw')
+# main_page = soup.find('div', id='search')
+
